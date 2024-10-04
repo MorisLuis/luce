8a40c07b3f140cf5f12a197d136aa27dd9547daa
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-04 16:12:20 -0600

    S / Land Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { Title } from "@/components/Title";
+import styles from "../styles/Home.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div >
-      <h1>Land Page</h1>
+    <div className={styles.Home}>
+      <Title title="LUCE" />
+
+      <div className={styles.imageContainer}>
+        <Image
+          src={`/images/armstrong-linear/Candil_schwung_armstronglinear_estudio.jpg`}
+          alt={'Imagen Land Page'}
+          layout="fill" // Esto permite que la imagen ocupe todo el contenedor
+          objectFit="cover" // Esto mantendrá la proporción de la imagen
+        />
+      </div>
     </div>
   );
 }
