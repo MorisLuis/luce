f0dc53933763edeb14fb96789c806fd6030051d1
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-04 15:56:44 -0600

    S / dropdown in navigation

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,16 +1,44 @@
-import Link from 'next/link'
-import React from 'react'
+"use client"; // Asegúrate de agregar esta línea al inicio del archivo
+
+import Link from 'next/link';
+import React, { useState } from 'react';
 import styles from "../styles/Navigation.module.scss";
 
+const categories = [
+    { id: 1, value: "Todos" },
+    { id: 2, value: "Schwung Design" },
+    { id: 3, value: "Colección Globe" },
+    { id: 4, value: "Candiles" },
+    { id: 5, value: "Lámparas arbotante" },
+];
+
 export const Nav = () => {
+    const [showDropDown, setShowDropDown] = useState(false);
+
     return (
         <nav className={styles.topNavigation}>
             <ul className={styles.container}>
                 <li><Link href={'/'}>LUCE</Link></li>
                 <li><Link href={'/'}>Inicio</Link></li>
-                <li><Link href={'/categories'}>Categorias</Link></li>
+                <li 
+                    onMouseEnter={() => setShowDropDown(true)} 
+                    onMouseLeave={() => setShowDropDown(false)}
+                >
+                    <span>Categorías</span>
+                    {showDropDown && (
+                        <ul className={styles.dropdown}>
+                            {categories.map(category => (
+                                <li key={category.id}>
+                                    <Link href={`/categories?category=${category.value}`}>
+                                        {category.value}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    )}
+                </li>
                 <li><Link href={'/contact'}>Contacto</Link></li>
             </ul>
         </nav>
-    )
+    );
 }
