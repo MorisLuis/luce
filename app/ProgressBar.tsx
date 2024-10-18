c9e9cc5b6ceab105f7edbfbbfac514eb555c5e39
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-17 19:21:01 -0600

    S / color variables

---
+++ app/ProgressBar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import NextNProgress from 'nextjs-progressbar';
+
+const ProgressBar = () => {
+    return (
+        <NextNProgress color="#EF476F" height={4} />
+    );
+};
+
+export default ProgressBar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-
   return (
     <html lang="en">
       <body>
