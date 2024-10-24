e350796d88dafc805578dd9ea7d0a4c97a06ff8f
“MorisLuis” <moradoluisenrique@gmail.com>
2024-10-24 11:33:05 -0600

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,6 @@ export default function Home() {
           <motion.div
             key="preloader"
             initial={{ opacity: 1 }}
-            //animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.5, delay: 0.1 }}
           >
